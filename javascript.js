96f4213f42f9e0cebc39781514c44228c62a7467
sarah-busch <119125518+sarah-busch@users.noreply.github.com>
2023-02-10 15:31:31 -0300

    Prompt and update player selection

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-const rock = 'Rock';
-const paper = 'Paper';
-const scissors = 'Scissors';
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
 
-let x = Math.random();
+// Human selection options
+
+const playerPrompt = prompt("Choose your weapon! Will it be rock, paper or scissors?");
+function getPlayerSelection(){
+if (playerPrompt.toLowerCase(`${playerPrompt}`) == "rock") {return(rock)}
+else if (playerPrompt.toLowerCase(`${playerPrompt}`) == "paper") {return(paper)}
+else if (playerPrompt.toLowerCase(`${playerPrompt}`) == "scissors") {return(scissors)}
+else {alert("Invalid selection.")};
+};
+
+const playerSelection = getPlayerSelection();
+
+// Computer selection options
+const x = Math.random();
 
 function getComputerChoice(x) {
     if (x < 0.33) {return(rock)}
@@ -10,4 +23,10 @@ function getComputerChoice(x) {
     else {return(scissors)};
 };
 
-console.log(getComputerChoice(x));
+alert("The computer has selected " + getComputerChoice(x));
+
+const computerSelection = getComputerChoice(x);
+
+// Comparing the results and declaring the winner
+
+
