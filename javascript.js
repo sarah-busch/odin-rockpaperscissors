9ed098f1c73eb846a4fad38b7c66bd8dd24eb07b
sarah-busch <119125518+sarah-busch@users.noreply.github.com>
2023-02-10 17:01:20 -0300

    Update result comparison and winner declaration

--- javascript.js
+++ javascript.js
@@ -5,14 +5,16 @@ const scissors = 'scissors';
 // Human selection options
 
 const playerPrompt = prompt("Choose your weapon! Will it be rock, paper or scissors?");
+
 function getPlayerSelection(){
 if (playerPrompt.toLowerCase(`${playerPrompt}`) == "rock") {return(rock)}
 else if (playerPrompt.toLowerCase(`${playerPrompt}`) == "paper") {return(paper)}
 else if (playerPrompt.toLowerCase(`${playerPrompt}`) == "scissors") {return(scissors)}
-else {alert("Invalid selection.")};
+else {alert("Invalid selection.")}
 };
 
 const playerSelection = getPlayerSelection();
+console.log("The user has selected " + playerSelection);
 
 // Computer selection options
 const x = Math.random();
@@ -23,10 +25,34 @@ function getComputerChoice(x) {
     else {return(scissors)};
 };
 
-alert("The computer has selected " + getComputerChoice(x));
-
 const computerSelection = getComputerChoice(x);
 
-// Comparing the results and declaring the winner
+alert("The computer has selected " + computerSelection);
+console.log("The computer has selected " + computerSelection);
+
+// Comparing the results 
+
+function finalResult(){
+if (playerSelection == rock && computerSelection == scissors ||
+    playerSelection == paper && computerSelection == rock ||
+    playerSelection == scissors && computerSelection == paper) {
+        return("win")
+    } else if (playerSelection == scissors && computerSelection == rock ||
+        playerSelection == rock && computerSelection == paper ||
+        playerSelection == paper && computerSelection == scissors) {
+            return("lose")
+        } else {
+            return("tie")
+        };
+    };
+
+// Declaring the winner
+
+if (finalResult() == "win" ) {alert("You win, " + playerSelection + " beats " + computerSelection + "!")}
+else if (finalResult() == "lose") {alert("You lose, " + computerSelection + " beats " + playerSelection + "!")}
+else {alert("It's a tie!")};
+
+console.log(finalResult());
+
 
 
