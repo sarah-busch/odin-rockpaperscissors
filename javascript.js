e60b3124808ff56c473adfd0c9ca4471025757f8
sarah-busch <119125518+sarah-busch@users.noreply.github.com>
2023-03-15 16:22:59 -0300

    Declare winner and change the DOM

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
 const playerArray = [];
 const compArray = [];
-const tieArray = [];
 
-let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
-let playerContainer = document.getElementById("playerScore");
-let compContainer = document.getElementById("compScore");
-let displayOutcome = document.getElementById("displayOutcome")
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerContainer = document.getElementById("playerScore");
+const compContainer = document.getElementById("compScore");
+const displayOutcome = document.getElementById("displayOutcome")
+const body = document.querySelector('body');
+const compImage = document.getElementById('compImage');
 
 
 function playRound() {
@@ -15,35 +16,50 @@ function playRound() {
     const paper = "paper";
     const scissors = "scissors";
     const x = Math.random();
+    const img = document.createElement("img");
 
     const playerSelection = `${this.id}`
 
-    console.log("Player: " + playerSelection);
+    function displayImage(src, width, height) {
+        const img = document.createElement("img");
+        img.src = src;
+        img.width = width;
+        img.height = height;
+        compImage.appendChild(img);
+       };
 
-        function getComputerSelection() 
-            {
-            if (x < (1/3)) {return(rock)}
-            else if (x < (2/3)) {return(paper)}
-            else {return(scissors)};
-            };
+    while (compImage.firstChild) {
+        compImage.removeChild(compImage.firstChild);
+    }
+    
+    function getComputerSelection() 
+        {
+        if (x < (1/3)) {
+            displayImage('./images/rock.png', 120, 80); 
+            return(rock)}
+        else if (x < (2/3)) {
+            displayImage('./images/paper.png', 70, 90);
+            return(paper)}
+        else {
+            displayImage('./images/scissors.png', 120, 60);
+            return(scissors)};
+        };
 
     const computerSelection = getComputerSelection();
-    console.log("Computer: " + computerSelection);
-
     
-    const win = "win";
-    const lose = "lose";
-    const tie = "tie";
+    const win = "PLAYER WINS";
+    const lose = "COMPUTER WINS";
+    const tie = "TIE";
          
-        function finalResult() 
-            {
-            if (playerSelection === computerSelection) {return(tie)} 
-            else if (playerSelection == scissors && computerSelection == rock ||
-                    playerSelection == rock && computerSelection == paper ||
-                    playerSelection == paper && computerSelection == scissors) 
-                    {return(lose)} 
-            else {return(win)}
-            };
+    function finalResult() 
+        {
+        if (playerSelection === computerSelection) {return(tie)} 
+        else if (playerSelection == scissors && computerSelection == rock ||
+            playerSelection == rock && computerSelection == paper ||
+            playerSelection == paper && computerSelection == scissors) 
+        {return(lose)} 
+        else {return(win)}
+        };
          
     const roundResult = finalResult();
 
@@ -52,85 +68,40 @@ function playRound() {
     let playerSum = 0;
     let compSum = 0;
 
-    if (roundResult == 'win') {
-        playerArray.push(1);
+    function tallyResults() 
+        {
         playerArray.forEach(item => {playerSum += item;});
-        compArray.forEach(item => {compSum += item;});
-        }
-        else if (roundResult == 'lose') {
-        compArray.push(1);
-        playerArray.forEach(item => {playerSum += item;});
-        compArray.forEach(item => {compSum += item;});
-        }
-        else {playerArray.forEach(item => {playerSum += item;});
         compArray.forEach(item => {compSum += item;});
         };
 
-    playerContainer.textContent = "Player score: " + playerSum;
-    compContainer.textContent = "Computer score: " + compSum;
+    if (roundResult == win) 
+        {
+        playerArray.push(1);
+        tallyResults(); 
+        }
+        else if (roundResult == lose) {
+        compArray.push(1);
+        tallyResults();}
+        else {tallyResults()};
+
+    playerContainer.textContent = "Player: " + playerSum;
+    compContainer.textContent = "Computer: " + compSum;
+
+    if (playerArray.length == 5) {
+        body.textContent = "CONGRATULATIONS YOU WIN";
+        body.style.color = "#4ff8a4";
+        body.style.textAlign = "center";
+        body.style.fontSize = "50px";
+        }
+        else if (compArray.length == 5) {
+        body.textContent= "COMPUTER WINS";
+        body.style.color = "#4ff8a4";
+        body.style.textAlign = "center";
+        body.style.fontSize = "50px";
+        } 
+        else {console.log("continue")};
 }
 
 rock.addEventListener('click', playRound);
 paper.addEventListener('click', playRound);
 scissors.addEventListener('click', playRound);
-
-
-
-
-/*let buttons = document.querySelectorAll('button');
-
-function playRound() {
-
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log("Player: " + button.id);
-
-            const rock = "rock";
-            const paper = "paper";
-            const scissors = "scissors";
-            const x = Math.random();
-
-            function getComputerSelection() 
-                {
-                if (x < (1/3)) {return(rock)}
-                else if (x < (2/3)) {return(paper)}
-                else {return(scissors)};
-                };
-
-             const computerSelection = getComputerSelection();
-             console.log("Computer: " + computerSelection);
-
-             const win = "win";
-             const lose = "lose";
-             const tie = "tie";
-         
-             function finalResult() 
-                {
-                 if (button.id === computerSelection) {return(tie)} 
-                 else if (button.id == scissors && computerSelection == rock ||
-                     button.id == rock && computerSelection == paper ||
-                     button.id == paper && computerSelection == scissors) 
-                     {return(lose)} 
-                 else {return(win)}
-                };
-         
-             const roundResult = finalResult();
-
-             if (roundResult == win) {playerArray.push(1)}
-             else if (roundResult == lose) {compArray.push(1)}
-             else {tieArray.push(0)}
-
-             let playerSum = 0 
-             let compSum = 0
-             
-             playerArray.forEach(item => {playerSum += item;});
-             compArray.forEach(item => {compSum += item;});
-             
-             console.log("Player score is " + playerSum);
-             console.log("Computer score is " + compSum);
-         });
-    });   
-}
-
-console.log(playRound());*/
-
